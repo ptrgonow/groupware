d7056dc445a69d372e80869007b846d92dff5b03
hnsangjin <uk96dhk1@gmail.com>
2024-06-05 18:20:43 +0900

    프로젝트 구조 (#1) - hsj

--- src/main/java/com/groupware/schedule/controller/SchdController.java
+++ src/main/java/com/groupware/schedule/controller/SchdController.java
@@ -1,6 +1,7 @@
 package com.groupware.schedule.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SchdController {
--- src/main/java/com/groupware/work/controller/WorkController.java
+++ src/main/java/com/groupware/work/controller/WorkController.java
@@ -1,7 +1,11 @@
 package com.groupware.work.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WorkController {
+
+    @GetMapping("/hr")
+    public String hr() {return "work/hr/main-hr";}
 }
