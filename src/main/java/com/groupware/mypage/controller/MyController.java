bf164bc0ae786c7bc70ea7b75e5223dec9445622
patrick <lusielar@naver.com>
2024-06-05 14:52:03 +0900

    프로젝트 빌드 (#1) - patrick

--- src/main/java/com/groupware/mypage/controller/MyController.java
+++ src/main/java/com/groupware/mypage/controller/MyController.java
@@ -1,7 +1,13 @@
 package com.groupware.mypage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyController {
+
+    @GetMapping("/mypage")
+    public String mypage() {
+        return "mypage/main-mypage";
+    }
 }
