6736cd80ce1794b53cd8ec0bfd04c00c0b74443c
DaHee <dahee215@naver.com>
2024-06-09 22:40:10 +0900

    프로젝트 구조 설계 (#1) - 다희

--- src/main/java/com/groupware/file/controller/FileController.java
+++ src/main/java/com/groupware/file/controller/FileController.java
@@ -1,7 +1,18 @@
 package com.groupware.file.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FileController {
+
+    @GetMapping("/file")
+    public String file() {
+        return "file/main-file";
+    }
+
+    @GetMapping("/file-vac")
+    public String vac() {
+        return "file/file-vac";
+    }
 }
