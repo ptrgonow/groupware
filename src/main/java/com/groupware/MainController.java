3ee0e02487a39accd2a9b3f1e66fa5cfea60fc36
patrick <lusielar@naver.com>
2024-06-10 10:53:41 +0900

    프로젝트 빌드 (#1) - patrick

--- src/main/java/com/groupware/MainController.java
+++ src/main/java/com/groupware/MainController.java
@@ -1,13 +1,20 @@
 package com.groupware;
 
+import com.groupware.user.dto.UserDTO;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model, HttpSession session) {
+        UserDTO user = (UserDTO) session.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("user", user);
+        }
         return "index";
     }
 }
