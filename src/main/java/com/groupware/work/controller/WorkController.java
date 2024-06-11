ab0ab261b4e1300f601a9dac0b25a5254a8521d4
patrick <lusielar@naver.com>
2024-06-11 12:09:17 +0900

    프로젝트 빌드 (#1) - patrick

--- src/main/java/com/groupware/work/controller/WorkController.java
+++ src/main/java/com/groupware/work/controller/WorkController.java
@@ -6,31 +6,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WorkController {
 
-@GetMapping("/fm")
-public String main() {
-    return "work/FM/main-finance";
-}
-@GetMapping("/fm/eform")
-    public String eform() {
-    return "work/FM/eform-finance";
+    @GetMapping("/fm")
+    public String main() {
+        return "work/FM/main-finance";
     }
-@GetMapping("/fm/eform-draft")
+
+    @GetMapping("/fm/eform")
+    public String eform() {
+        return "work/FM/eform-finance";
+    }
+    @GetMapping("/fm/eform-draft")
     public String eformDraft() {
-    return "work/FM/eform-draft";
-}
+        return "work/FM/eform-draft";
+    }
 
-@GetMapping("/hr")
-public String hr() {
-    return "work/hr/main-hr";
-}
+    @GetMapping("/hr")
+    public String hr() {
+        return "work/hr/main-hr";
+    }
 
-@GetMapping("/registerPage")
-public String registerPage() {
-    return "work/hr/hr-register";
-}
+    @GetMapping("/registerPage")
+    public String registerPage() {
+        return "work/hr/hr-register";
+    }
 
     @GetMapping("/dev")
-    public String dev() {return "work/dev/main-dev";}
+    public String dev() {
+        return "work/dev/main-dev";
+    }
 
 }
 
